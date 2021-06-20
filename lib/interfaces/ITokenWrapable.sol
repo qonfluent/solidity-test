bb215c9f50dcee6a71baeb48c8439c70088da5b1
Qonfluent <>
2021-06-19 20:04:05 -0700

    Added interface for wrappable tokens

---
+++ lib/interfaces/ITokenWrapable.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./IERC20.sol";
+
+interface ITokenWrapable is IERC20 {
+	// -- EVENTS --
+
+	event Wrap (
+		address indexed owner,
+		uint256 amount
+	);
+
+	event Unwrap (
+		address indexed owner,
+		uint256 amount
+	);
+
+	// -- VIEW FUNCTIONS --
+
+	function getWrappedToken () external view returns (address);
+
+	function getTresasury () external view returns (address);
+
+	// -- EXTERNAL FUNCTIONS --
+
+	function wrap (
+		uint256 amount
+	) external returns (uint256);
+
+	function unwrap (
+		uint256 amount
+	) external returns (uint256);
+}
